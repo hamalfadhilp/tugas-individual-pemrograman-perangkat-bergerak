065ffb6e60e7d014dea3d7f8c15793cb582c037b
Hamal Fadhil <hamalzoldyk55@gmail.com>
2023-06-15 18:25:23 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# tugas-individual-pemrograman-perangkat-bergerak
---
+++ bab 4.4/app/src/main/java/com/example/bab_44/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.bab_44
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun openProfile(view: View) {
+        val intent = Intent(this, ProfileActivity::class.java)
+        startActivity(intent)
+    }
+
+    fun openSettings(view: View) {
+        val intent = Intent(this, SettingsActivity::class.java)
+        startActivity(intent)
+    }
+}
